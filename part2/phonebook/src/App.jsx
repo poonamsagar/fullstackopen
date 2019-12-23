07beed5ffe72388187e12f8fd4c82c15d5437d9a
Poonam Sagar <poonamsagar9212@gmail.com>
2019-12-23 21:09:38 +1100

    The Phonebook Step3 completed.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,26 +1,41 @@
 import React, { useState } from "react";
 
-const Person = ({ name }) => <div>{name}</div>;
+const Person = ({ name, number }) => (
+  <div>
+    {name} {number}
+  </div>
+);
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "987654321" }
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const row = () =>
-    persons.map(person => <Person key={person.name} name={person.name} />);
+    persons.map(person => (
+      <Person key={person.name} name={person.name} number={person.number} />
+    ));
 
   const addPerson = event => {
     event.preventDefault();
-    const newPerson = { name: newName };
-    if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to the phonebook`);
-    } else {
-      setPersons(persons.concat(newPerson));
-      setNewName("");
+    if (newName.length) {
+      const newPerson = { name: newName, number: newNumber };
+      if (persons.some(person => person.name === newName)) {
+        alert(`${newName} is already added to the phonebook`);
+      } else {
+        setPersons(persons.concat(newPerson));
+        setNewName("");
+        setNewNumber("");
+      }
     }
   };
-  const changeHandler = event => {
+  const nameChangeHandler = event => {
     setNewName(event.target.value);
+  };
+  const numberChangeHandler = event => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -28,7 +43,11 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={changeHandler} />
+          name: <input value={newName} onChange={nameChangeHandler} required />
+        </div>
+        <div>
+          number:{" "}
+          <input value={newNumber} onChange={numberChangeHandler} required />
         </div>
         <div>
           <button type="submit">add</button>
