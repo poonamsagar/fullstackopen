f204aa39235c41cd994aa000c8e9adfae37bea17
Poonam Sagar <poonamsagar9212@gmail.com>
2019-12-26 19:26:29 +1100

    The Phonebook Step4 completed.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -8,13 +8,21 @@ const Person = ({ name, number }) => (
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "987654321" }
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" }
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-
+  const [searchTerm, setSearchTerm] = useState("");
+  const filterResult = !searchTerm
+    ? persons
+    : persons.filter(person =>
+        person.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
   const row = () =>
-    persons.map(person => (
+    filterResult.map(person => (
       <Person key={person.name} name={person.name} number={person.number} />
     ));
 
@@ -37,10 +45,17 @@ const App = () => {
   const numberChangeHandler = event => {
     setNewNumber(event.target.value);
   };
-
+  const filterChangeHandler = event => {
+    setSearchTerm(event.target.value);
+  };
   return (
     <>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with{" "}
+        <input value={searchTerm} onChange={filterChangeHandler} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={nameChangeHandler} required />
